2a1b7ceaff5b93a82d30f74f2e87854171341d34
fresh-14 <fresh_14@mail.ru>
2021-04-17 09:36:09 +0300

    fix switch-case

--- ImageReaderFactory.java
+++ ImageReaderFactory.java
@@ -2,25 +2,17 @@ import common.*;
 
 public class ImageReaderFactory {
     public static ImageReader getImageReader(ImageTypes imageType) {
-        ImageReader imageReader;
 
         if (imageType == null) {
             throw new IllegalArgumentException("Неизвестный тип картинки");
         }
 
-        switch (imageType) {
-            case BMP:
-                imageReader = new BmpReader();
-                break;
-            case JPG:
-                imageReader = new JpgReader();
-                break;
-            case PNG:
-                imageReader = new PngReader();
-                break;
-            default:
-                throw new IllegalArgumentException("Неизвестный тип картинки");
-        }
+        ImageReader imageReader = switch (imageType) {
+            case BMP -> new BmpReader();
+            case JPG -> new JpgReader();
+            case PNG -> new PngReader();
+            default -> throw new IllegalArgumentException("Неизвестный тип картинки");
+        };
 
         return imageReader;
     }
